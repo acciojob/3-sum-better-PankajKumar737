509c8a8a7ade4ed35a2d2a94d611b7484a2cf39c
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-03 20:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
+	var sum =0;
+	for(var i=0; i<arr.length; i++){
+		if(arr[i] == target){
+			sum = arr[i-1]+arr[i]+arr[i-2];
+		}
+		return sum;
+	}
   
 }
 
